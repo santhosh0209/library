a19c8daed130eabb128e5a427a270ae458fd0d8a
santhosh0209 <bluyblack0902@gmail.com>
2020-06-05 16:29:33 +0530

    initial commit

--- libraryms/settings.py
+++ libraryms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$gk-dl%dz#y$$niy4z%y65x54-suvq5ds4zyh3s1$u)c8a=eyb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
